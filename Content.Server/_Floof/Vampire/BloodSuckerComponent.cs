985a058933249f05776c5a5b7fbe2f719e75baad
Sydney Grayy <sydneygray64@protonmail.com>
2026-08-23 20:32:45 -0700

    Reset BloodSuckerComponent to Euphoria's master branch

--- Content.Server/_Floof/Vampire/BloodSuckerComponent.cs
+++ Content.Server/_Floof/Vampire/BloodSuckerComponent.cs
@@ -1,59 +1,43 @@
-using Content.Shared.Chemistry.Reagent;
-using Robust.Shared.Audio;
-using Robust.Shared.Prototypes;
+﻿namespace Content.Server._Floof.Vampire;
 
-
-namespace Content.Server._Floof.Vampire
+[RegisterComponent]
+public sealed partial class BloodSuckerComponent : Component
 {
-    [RegisterComponent]
-    public sealed partial class BloodSuckerComponent : Component
-    {
-        /// <summary>
-        /// How much to succ each time we succ.
-        /// </summary>
-        [DataField("unitsToSucc")]
-        public float UnitsToSucc = 20f;
+    /// <summary>
+    /// How much to succ each time we succ.
+    /// </summary>
+    [DataField("unitsToSucc")]
+    public float UnitsToSucc = 20f;
 
-        /// <summary>
-        /// The time (in seconds) that it takes to succ an entity.
-        /// </summary>
-        [DataField, ViewVariables(VVAccess.ReadWrite)]
-        public TimeSpan Delay = TimeSpan.FromSeconds(4);
-// Hardlight unused - start
-        // ***INJECT WHEN SUCC***
+    /// <summary>
+    /// The time (in seconds) that it takes to succ an entity.
+    /// </summary>
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public TimeSpan Delay = TimeSpan.FromSeconds(4);
 
-        /// <summary>
-        /// Whether to inject chems into a chemstream when we suck something.
-        /// </summary>
-        // [DataField("injectWhenSucc")]
-        // public bool InjectWhenSucc = false;
+    // ***INJECT WHEN SUCC***
 
-        /// <summary>
-        /// How many units of our injected chem to inject.
-        /// </summary>
-        // [DataField("unitsToInject")]
-        // public float UnitsToInject = 5;
+    /// <summary>
+    /// Whether to inject chems into a chemstream when we suck something.
+    /// </summary>
+    [DataField("injectWhenSucc")]
+    public bool InjectWhenSucc = false;
 
-        /// <summary>
-        /// Which reagent to inject.
-        /// </summary>
-        // [DataField("injectReagent")]
-        // public string InjectReagent = "";
+    /// <summary>
+    /// How many units of our injected chem to inject.
+    /// </summary>
+    [DataField("unitsToInject")]
+    public float UnitsToInject = 5;
 
-        /// <summary>
-        /// Whether we need to web the thing up first...
-        /// </summary>
-        // [DataField("webRequired")]
-        // public bool WebRequired = false;
+    /// <summary>
+    /// Which reagent to inject.
+    /// </summary>
+    [DataField("injectReagent")]
+    public string InjectReagent = "";
 
-        /// <summary>
-        ///     DEN: Used to track BloodExaminer, but only if it is added by this component.
-        /// </summary>
-        // public BloodExaminerComponent? AddedBloodExaminer;
-// Hardlight unused - end
-        /// <summary>
-        /// The sound made when drinking blood.
-        /// </summary>
-        public SoundSpecifier DrinkSound = new SoundPathSpecifier("/Audio/Items/drink.ogg");
-    }
+    /// <summary>
+    /// Whether we need to web the thing up first...
+    /// </summary>
+    [DataField("webRequired")]
+    public bool WebRequired = false;
 }
